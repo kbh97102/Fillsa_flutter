3fac4feee6547c3b036e2dc6c0239d0de5557a5a
arakene <arakene_dev@naver.com>
2025-09-07 15:21:32 +0900

    fix: assets 경로 수정

--- lib/presentation/util/typo.dart
+++ lib/presentation/util/typo.dart
@@ -63,125 +63,125 @@ class FillsaTypo extends ThemeExtension<FillsaTypo> {
 final fillsaTypoData = FillsaTypo(
   heading1: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 32.0,
     height: 1.5,
     color: grey700,
   ), // 48 / 32
   heading2: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 28.0,
     height: 1.5,
     color: grey700,
   ), // 42 / 28
   heading3: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 24.0,
     height: 1.5,
     color: grey700,
   ), // 36 / 24
   heading4: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 20.0,
     height: 1.5,
     color: grey700,
   ), // 30 / 20
   subtitle1: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 16.0,
     height: 1.5,
     color: grey700,
   ), // 24 / 16
   subtitle2: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 14.0,
     height: 1.5,
     color: grey700,
   ), // 21 / 14
   body1: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 20.0,
     height: 1.5,
     color: grey700,
   ), // 30 / 20
   body2: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 16.0,
     height: 1.5,
     color: grey700,
   ), // 24 / 16
   body3: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 14.0,
     height: 1.5,
     color: grey700,
   ), // 21 / 14
   body4: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 12.0,
     height: 1.5,
     color: grey700,
   ), // 18 / 12
   buttonLargeBold: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 20.0,
     color: grey700,
   ),
   buttonLargeNormal: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 20.0,
     color: grey700,
   ),
   buttonMediumBold: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 16.0,
     color: grey700,
   ),
   buttonMediumNormal: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 16.0,
     color: grey700,
   ),
   buttonSmallBold: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 14.0,
     color: grey700,
   ),
   buttonSmallNormal: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 14.0,
     color: grey700,
   ),
   buttonXSmallBold: TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 12.0,
     color: grey700,
   ),
   buttonXSmallNormal: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'Pretendard',
+    fontFamily: 'pretendard',
     fontSize: 12.0,
     color: grey700,
   ),
   quote: TextStyle(
     fontWeight: FontWeight.normal,
-    fontFamily: 'GangwonEduAll',
+    fontFamily: 'gangwoneduall',
     fontSize: 16.0,
     height: 1.5,
     color: grey700,
